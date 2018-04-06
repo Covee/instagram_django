8051bdc2c5b01b18986084116896149c9aa6ba39
Covee <uniqtop91@gmail.com>
2018-04-07 00:37:10 +0900

    로그인 로그아웃 구현

--- member/views.py
+++ member/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as django_login, logout as django_logout, authenticate
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login(request):
+	if request.method == 'POST':
+		login_form = LoginForm(request.POST)
+		if login_form.is_valid():
+			username = login_form.cleaned_data['username']
+			password = login_form.cleaned_data['password']
+
+			user = authenticate(
+				username=username,
+				password=password
+			)
+			if user:
+				django_login(request, user)
+				return redirect('post:post_list')
+			
+			login_form.add_error(None, '아이디 또는 비밀번호가 올바르지 않습니다.')
+	else:
+		login_form = LoginForm()
+	context = {
+		'login_form': login_form,
+	}
+	return render(request, 'member/login.html', context)
+
+def logout(request):
+	django_logout(request)
+	return redirect('post:post_list')
