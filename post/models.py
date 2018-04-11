18d49701229f43f55d8a26e65c1972fb339c5d31
Cobee <cobee@Cobeeui-MacBook-Pro.local>
2018-04-11 23:51:28 +0900

    post datetime 추가

--- post/models.py
+++ post/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL)
 	photo = models.ImageField(upload_to='post')
+	created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
 
 	def __str__(self):
 		return f'Post (PK: {self.pk}, Author: {self.author.username})'
